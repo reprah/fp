6be4d352cf18b74400f1f32e5fe9b331ddd92b50
py-in-the-sky <ryanmwise1@gmail.com>
2014-10-04 09:55:48 -0700

    Update week-2-100-doors.hs

--- week-2-100-doors.hs
+++ week-2-100-doors.hs
@@ -1,11 +1,15 @@
 doors = [1..100]
 
+
 countDoorsOpen :: [Int] -> Int
 countDoorsOpen doorIndices = (sum . map isOpen) doorIndices
 
+
 isOpen :: Int -> Int  -- returns 1 for open, 0 for closed
-isOpen n = mod nVisits 2
+isOpen n = mod nVisits 2  -- door left open if 'visited' an odd number of times
     where nVisits = (sum . mapDivisibilityTest) [1..n]
           mapDivisibilityTest = map (\n2 -> if mod n n2 == 0 then 1 else 0)
+          -- an integer n2 'visits' n if and only if n2 divides n
+
 
 test = 10 == (countDoorsOpen doors)
