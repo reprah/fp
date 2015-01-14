d6108601641c8cf6608def680170a9a84dd8141f
Harper Henn <harper.henn@gmail.com>
2015-01-13 22:18:48 -0800

    If you squint hard enough, you can make yourself believe I wrote this in Clojure

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Exploring functional programming with a co-worker
 * week 1: https://www.hackerrank.com/challenges/super-digit
 * week 2: http://rosettacode.org/wiki/100_doors
 * week 3: https://www.hackerrank.com/challenges/messy-medians
+* week 4: http://www.ctl.ua.edu/math103/euler/ifagraph.htm
---
+++ eulerian_paths.rb
@@ -0,0 +1,96 @@
+# require 'pry'
+
+graph = [
+  [:a, :e], [:a, :b], [:a, :c],
+  [:e, :f], [:c, :f], [:f, :d],
+  [:d, :c], [:b, :c], [:b, :d],
+  [:d, :b]
+]
+
+graph2 = [
+  [:d, :c], [:a, :c], [:b, :d],
+  [:d, :b], [:b, :c], [:c, :f],
+  [:e, :f], [:a, :b], [:a, :e],
+  [:f, :d]
+] 
+
+graph3 = [
+  [:a, :f], [:a, :e], [:a, :e], [:a, :b],
+  [:f, :d], [:f, :d], [:f, :c], [:e, :c],
+  [:e, :b], [:e, :d], [:b, :c], [:d, :c]
+]
+
+def dfs(graph, start_node, visited_nodes = [])
+  if not visited_nodes.include?(start_node)
+    visited_nodes.push(start_node)
+    neighbors = graph.select { |edge|
+      edge.include?(start_node)
+    }.flatten.reject { |edge|
+      edge == start_node
+    }
+    neighbors.each { |neighbor|
+      dfs(graph, neighbor, visited_nodes)
+    }
+  end
+  visited_nodes
+end
+
+def find_bridges(graph)
+  all_nodes = graph.flatten.uniq
+  return graph if graph.size == 1
+  graph.reject { |edge|
+    clone = graph.clone
+    clone = clone.reject { |e| e == edge }
+    found_nodes = dfs(clone, clone[0][0])
+    found_nodes.size == all_nodes.size
+  }
+end
+
+def vertices_with_odd_degrees(graph)
+  vertices_with_degrees = Hash.new(0)
+  vertices = graph.flatten.uniq
+  vertices.each { |vertex|
+    degrees = graph.select { |edge| edge.include?(vertex) }.count
+    vertices_with_degrees[vertex] = degrees
+  }
+  vertices_with_degrees.select { |vertex, degrees| degrees.odd? }
+end
+
+def has_eulerian_path?(graph)
+  vertices_with_odd_degrees(graph).count == 2
+end
+
+def eulerian_path(graph)
+  if has_eulerian_path?(graph)
+    path = []
+    start_node = vertices_with_odd_degrees(graph).keys[0]
+    current_edge = graph.detect { |edge| edge.include?(start_node) }
+    next_node = nil
+    until graph.empty? do
+      if next_node
+        current_node = next_node
+        next_node = current_edge.reject { |n| n == next_node }[0]
+      else
+        current_node = start_node
+        next_node = current_edge.reject { |n| n == start_node }[0]
+      end
+      path.push(current_node)
+      graph = graph.reject { |edge| edge == current_edge }
+      next_edge_candidates = graph.select { |edge| edge.include?(next_node) }
+      bridges = find_bridges(graph)
+      non_bridges = next_edge_candidates.reject { |edge| bridges.include?(edge) }
+      if non_bridges.any?
+        next_edge = non_bridges[0]
+      else
+        next_edge = next_edge_candidates[0]
+      end
+      current_edge = next_edge
+    end
+    path.push(next_node)
+    path
+  else
+    []
+  end
+end
+
+p eulerian_path(graph2)
