13e9b48d1af4c0f87c1d674b3bae7117da96d1ad
py-in-the-sky <ryanmwise1@gmail.com>
2015-01-30 16:03:59 -0800

    Update eulerian_paths.py

--- eulerian_paths.py
+++ eulerian_paths.py
@@ -1,12 +1,27 @@
-GRAPH1 = """
-    A -> BEEF
-    B -> ACCE
-    C -> BBDE
-    D -> CEFF
-    E -> AABCDF
-    F -> ADDE
+"""
+An Euler path (EP) is a path that uses every edge of a graph exactly once and
+starts and ends at different vertices.
+
+To have an EP, the graph must have exactly two nodes of odd degree:
+an EP will visit every edge as well as exit any node the same
+number of times it enters the node, except for the starting and ending nodes,
+which implies that the starting and ending nodes have odd degree -- and only
+these nodes.
+
+To have a chance of explicitly following an EP, we must choose as our starting
+point a node of odd degree.
 """
 
+GRAPH1 = """
+    A -> BEEF
+    B -> ACE
+    C -> BDEF
+    D -> CEFF
+    E -> AABCD
+    F -> ACDD
+"""
+
+### Parsing
 
 def parse_graph(graph):
     lines = (line for line in graph.strip().split('\n') if line)
@@ -14,72 +29,84 @@ def parse_graph(graph):
     return {node: neighbors for node,neighbors in parsed_lines}
 
 
-def traverse_edge(edge, graph):
-    def _update_nodes(graph):
-        for node,neighbors in graph.iteritems():
-            if node == from_node:
-                yield node, neighbors.replace(to_node, '', 1)
-            elif node == to_node:
-                yield node, neighbors.replace(from_node, '', 1)
-            else:
-                yield node, neighbors
-
-    from_node, to_node = edge
-    assert from_node in graph[to_node] and to_node in graph[from_node]
-    new_graph = {node: neighbors for node,neighbors in _update_nodes(graph)}
-    return new_graph
-
-
-def is_bridge(edge, graph):
-    new_graph = traverse_edge(edge, graph)
-    return not is_connected(new_graph)
-
+### Helpers
 
 def is_connected(graph):
-    starting_node = graph.keys()[0]
+    connected_nodes = [node for node,neighbors in graph.iteritems() if neighbors]
+    starting_node = connected_nodes[0]
     to_visit_queue = [ starting_node ]
     visited = set()
 
-    while to_visit_queue:  # search to accumulate 'visited' graph nodes
+    while to_visit_queue:  # search accumulates 'visited' graph nodes
         node = to_visit_queue.pop()
-        visited.add(node)
-        next_nodes = [n for n in graph[node] if n not in visited]
-        to_visit_queue += next_nodes
+        if node not in visited:
+            visited.add(node)
+            to_visit_queue += list(graph[node])
 
-    return visited == set(graph.keys())
+    return visited == set(connected_nodes)
+
+
+def is_odd(n): return n % 2 == 1
+
+
+def odd_nodes(graph):
+    return [node for node,neighbors in graph.iteritems() if is_odd(len(neighbors))]
+
+
+def edges(node, neighbors): return (node + n for n in neighbors)
+
+
+def all_edges(graph):
+    return (edge for node,neighbors in graph.iteritems()
+            for edge in edges(node, neighbors))
+
+
+def non_bridges(edges, graph):
+    for edge in edges:
+        start_node = edge[0]
+        _, new_graph = walk_edge(graph, start_node, edge)
+        if is_connected(new_graph):
+            yield edge
+
+
+def walk_edge(graph, start_node, edge):
+    end_node = next(node for node in edge if node != start_node)
+    new_graph = graph.copy()
+    new_graph[start_node] = new_graph[start_node].replace(end_node, '', 1)
+    new_graph[end_node]   = new_graph[end_node].replace(start_node, '', 1)
+    return end_node, new_graph
+
+
+def walk_non_bridges_first(graph, start_node, path=[]):
+    neighbors = graph[start_node]
+
+    if not neighbors:  # can't continue path
+        return path
+
+    available_edges = edges(start_node, neighbors)
+    next_edge = next(non_bridges(available_edges, graph), next(available_edges))
+
+    new_path = path + [next_edge]
+    new_node, new_graph = walk_edge(graph, start_node, next_edge)
+    return walk_non_bridges_first(new_graph, new_node, new_path)
+
+
+### Top-Level
+
+def can_find_eulerian_path(graph):
+    start_node = odd_nodes(graph)[0]
+    path = walk_non_bridges_first(graph, start_node)
+    edges_in_path = set(frozenset(edge) for edge in path)
+    all_graph_edges = set(frozenset(edge) for edge in all_edges(graph))
+    return edges_in_path == all_graph_edges
+
+
+def has_two_odd_nodes(graph): return len(odd_nodes(graph)) == 2
 
 
 def has_eulerian_path(graph):
-    def _next_edge(current_node):
-        if not graph[current_node]:
-            return None
-        default_edge = current_node + graph[current_node][0]
-        candidate_edges = (current_node + next_node for next_node in graph[current_node])
-        non_bridges = (e for e in candidate_edges if not is_bridge(e, graph))
-        return frozenset(next(non_bridges, default_edge))
-
-    def _traverse(graph, node, traversed_edges):
-        edge = _next_edge(node)
-        if not edge:
-            return node, traversed_edges
-        else:
-            new_graph = traverse_edge(edge, graph)
-            new_node  = next(n for n in edge if n != node)
-            return _traverse(new_graph, new_node, traversed_edges + [edge])
-
-    starting_node = next((n for n,neighbors in graph.iteritems() if neighbors), None)
-    if not starting_node:
-        return len(graph.keys()) in (0, 1)
-
-    all_graph_edges = all_edges(graph)
-    end_node, traversed_edges = _traverse(graph, starting_node, [])
-    return end_node == starting_node and set(traversed_edges) == all_graph_edges
-
-
-def all_edges(graph):  #, one_node=None):
-    # graph_items = [(one_node, graph[one_node])] if one_node else graph.iteritems()
-    edges = (node+node2 for node,neighbors in graph.iteritems() for node2 in neighbors)
-    return set(frozenset(e) for e in edges)
+    assert len(graph.keys()) > 1, 'Not interesting'
+    return has_two_odd_nodes(graph) and can_find_eulerian_path(graph)
 
 
 def main(graph=GRAPH1):
@@ -88,37 +115,7 @@ def main(graph=GRAPH1):
 
 
 def tests():
-    graph = """
-        a -> bbc
-        b -> aac
-        c -> abd
-        d -> ce
-        e -> d
-    """
-    g = parse_graph(graph)
-    assert traverse_edge('ab', g) == {'a': 'bc', 'b': 'ac', 'c': 'abd', 'd': 'ce', 'e': 'd'}
-
-    g2 = g.copy()
-    g2['d'] = 'c'
-    g2['e'] = ''
-    assert traverse_edge('de', g) == g2
-    assert is_connected(g)
-    assert not is_connected(g2)
-
-    assert is_bridge('de', g)
-    assert not is_bridge('ab', g)
-
-    assert all_edges(g) == set([frozenset(['e', 'd']), frozenset(['c', 'b']),
-                                frozenset(['a', 'c']), frozenset(['a', 'b']),
-                                frozenset(['c', 'd'])])
-    assert all_edges(g2) == set([frozenset(['c', 'b']), frozenset(['a', 'c']),
-                                 frozenset(['a', 'b']), frozenset(['c', 'd'])])
-
-    assert not has_eulerian_path(g)
-    assert has_eulerian_path(parse_graph(GRAPH1))
-
     assert main()
-
     return 'tests pass!'
 
 
